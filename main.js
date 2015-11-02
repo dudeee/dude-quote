a21b14bdd1fe8733b112cf3679d9dfcb9ff82109
Mahdi Dibaiee <mdibaiee@aol.com>
2015-11-02 15:00:15 +0330

    chore: upgrade to babel 6

---
+++ main.js
@@ -0,0 +1,32 @@
+import unirest from 'unirest';
+
+export default bot => {
+  let data = bot.data.quote;
+
+  bot.agenda.define('quote', (job, done) => {
+    unirest.get('http://api.theysaidso.com/qod.json').end(response => {
+      if (!response.body.contents) return;
+
+      let quote = response.body.contents.quotes[0];
+      let message = quote.quote + ` _–${quote.author}_`;
+      bot.sendMessage(data.target, message);
+
+      done();
+    });
+  });
+
+  bot.agenda.on('ready', () => {
+    let job = bot.agenda.create('quote');
+
+    job.repeatAt(data.repeatAt);
+
+    bot.agenda.on('complete', function(job) {
+      console.log("Job %s finished", job.attrs.name);
+    });
+    bot.agenda.on('start', function(job) {
+      console.log("Job %s started", job.attrs.name);
+    });
+
+    job.save();
+  })
+}
